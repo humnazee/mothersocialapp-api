b940a2c3c73b3ae541426fb42fb5b9f4f5c1c11a
humnazee <zeeshan.humna@hotmail.com>
2023-06-29 08:58:47 +0930

    final project submission

--- client/js/components/post_list.js
+++ client/js/components/post_list.js
@@ -173,91 +173,97 @@ function updateLikeCount(postId, isLiked) {
 
 
 
-function renderPostList() {
-  const postListDOM = document.querySelector('#page');
+// function renderPostList() {
+//   const postListDOM = document.querySelector('#page');
 
-  postListDOM.innerHTML = state.posts.map(post => `
-    <section class="post" data-id="${post.id}">
-      <header>
-        <h2 onClick="renderPostInfo(${post.id})">${post.title}</h2>
-      </header>
-      <div class="card">
-        <div class="card-img">
-          <img src="${post.image_url}" alt="">
-        </div>
-        <div class="card-body">
-          <h4 onClick="renderPostInfo(${post.id})">${post.title}</h4>
-          <p>${post.content}</p>
-          <div class="post-footer">
-            <button id="likeButton-${post.id}" class="like-button" onclick="handleLikeButtonClick(${post.id})">
-              Like
-            </button>
-            <span id="likeCount-${post.id}" class="like-count">${post.likes || 0}</span>
-          </div>
-        </div>
-        <div class="card-footer">
-          <form class="comment-form" onsubmit="addComment(event, ${post.id})">
-            <input type="text" name="comment" placeholder="Add a comment">
-            <button type="submit">Submit</button>
-          </form>
-          <div id="comments-${post.id}" class="comments">
-            <!-- Comments will be rendered here -->
-          </div>
-        </div>
-      </div>
-    </section>
-  `).join('');
+//   postListDOM.innerHTML = state.posts.map(post => `
+//     <section class="post" data-id="${post.id}">
+//       <header>
+//         <h2 onClick="renderPostInfo(${post.id})">${post.title}</h2>
+//       </header>
+//       <div class="card">
+//         <div class="card-img">
+//           <img src="${post.image_url}" alt="">
+//         </div>
+//         <div class="card-body">
+//           <h4 onClick="renderPostInfo(${post.id})">${post.title}</h4>
+//           <p>${post.content}</p>
+//           <div class="post-footer">
+//             <button id="likeButton-${post.id}" class="like-button" onclick="handleLikeButtonClick(${post.id})">
+//               Like
+//             </button>
+//             <span id="likeCount-${post.id}" class="like-count">${post.likes || 0}</span>
+//           </div>
+//         </div>
+//         <div class="card-footer">
+//           <form class="comment-form" onsubmit="addComment(event, ${post.id})">
+//             <input type="text" name="comment" placeholder="Add a comment">
+//             <button type="submit">Submit</button>
+//           </form>
+//           <div id="comments-${post.id}" class="comments">
+//             <!-- Comments will be rendered here -->
+//           </div>
+//         </div>
+//       </div>
+//     </section>
+//   `).join('');
 
-  renderEmptyCommentList();
-}
+//   renderEmptyCommentList();
+// }
 
-function handleLikeButtonClick(postId) {
-  const likeButton = document.getElementById(`likeButton-${postId}`);
+// function handleLikeButtonClick(postId) {
+//   const likeButton = document.getElementById(`likeButton-${postId}`);
 
-  // Check if the post is already liked
-  const isLiked = likeButton.classList.contains('liked');
+//   // Check if the post is already liked
+//   const isLiked = likeButton.classList.contains('liked');
 
-  if (isLiked) {
-    fetch(`/api/posts/${postId}/unlike`, {
-      method: 'POST',
-    })
-      .then((res) => {
-        if (res.ok) {
-          likeButton.classList.remove('liked');
-          updateLikeCount(postId, false); // Update the like count
-        } else {
-          throw new Error('Failed to unlike post');
-        }
-      })
-      .catch((error) => {
-        console.error('Error unliking post:', error);
-      });
-  } else {
-    fetch(`/api/posts/${postId}/like`, {
-      method: 'POST',
-    })
-      .then((res) => {
-        if (res.ok) {
-          likeButton.classList.add('liked');
-          updateLikeCount(postId, true); // Update the like count
-        } else {
-          throw new Error('Failed to like post');
-        }
-      })
-      .catch((error) => {
-        console.error('Error liking post:', error);
-      });
-  }
-}
+//   if (isLiked) {
+//     fetch(`/api/posts/${postId}/unlike`, {
+//       method: 'POST',
+//     })
+//       .then((res) => {
+//         if (res.ok) {
+//           likeButton.classList.remove('liked');
+//           updateLikeCount(postId, false); // Update the like count
+//         } else {
+//           throw new Error('Failed to unlike post');
+//         }
+//       })
+//       .catch((error) => {
+//         console.error('Error unliking post:', error);
+//       });
+//   } else {
+//     fetch(`/api/posts/${postId}/like`, {
+//       method: 'POST',
+//     })
+//       .then((res) => {
+//         if (res.ok) {
+//           likeButton.classList.add('liked');
+//           updateLikeCount(postId, true); // Update the like count
+//         } else {
+//           throw new Error('Failed to like post');
+//         }
+//       })
+//       .catch((error) => {
+//         console.error('Error liking post:', error);
+//       });
+//   }
+// }
 
-// Function to update the like count dynamically
-function updateLikeCount(postId, isLiked) {
-  const likeCountElement = document.getElementById(`likeCount-${postId}`);
-  const currentLikeCount = parseInt(likeCountElement.textContent);
+// // Function to update the like count dynamically
+// function updateLikeCount(postId, isLiked) {
+//   const likeCountElement = document.getElementById(`likeCount-${postId}`);
+//   const currentLikeCount = parseInt(likeCountElement.textContent);
 
-  if (isLiked) {
-    likeCountElement.textContent = isNaN(currentLikeCount) ? 1 : currentLikeCount + 1;
-  } else {
-    likeCountElement.textContent = isNaN(currentLikeCount) ? 0 : currentLikeCount - 1;
-  }
-}
+//   if (isLiked) {
+//     likeCountElement.textContent = isNaN(currentLikeCount) ? 1 : currentLikeCount + 1;
+//   } else {
+//     likeCountElement.textContent = isNaN(currentLikeCount) ? 0 : currentLikeCount - 1;
+//   }
+// }
+
+
+
+
+
+
--- db/schema.sql
+++ db/schema.sql
@@ -20,8 +20,19 @@ CREATE TABLE posts (
 );
 INSERT INTO posts(title, content, image_url, created_at)
 VALUES
-  ('Beach fun', 'Enjoying holidays with kids on my favourite beach ', 'https://images.pexels.com/photos/1231365/pexels-photo-1231365.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', CURRENT_TIMESTAMP );
-  
+  ('Decorating chritmas tree', ' happy family', 'https://images.pexels.com/photos/3303614/pexels-photo-3303614.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', CURRENT_TIMESTAMP ),
+
+   ('Art activity','Help your child in healthy activities','https://images.pexels.com/photos/16485567/pexels-photo-16485567/free-photo-of-mother-with-two-kids-sitting-in-a-playroom.jpeg', CURRENT_TIMESTAMP ),
+
+   ('Stay fit and shine bright', 'Being a mother is learning about strengths', 'https://images.pexels.com/photos/5445218/pexels-photo-5445218.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', CURRENT_TIMESTAMP ),
+
+   ('Me time', 'selfcare isnt selfish', 'https://images.pexels.com/photos/6487208/pexels-photo-6487208.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', CURRENT_TIMESTAMP ),
+
+   (' Beach day', 'All you need is family', 'https://images.pexels.com/photos/1231365/pexels-photo-1231365.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', CURRENT_TIMESTAMP ),
+
+   (' Stay fit and active', 'kids-they do what they see so .', 'https://images.pexels.com/photos/6951846/pexels-photo-6951846.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', CURRENT_TIMESTAMP ),
+
+   ('stay fit with babies', 'Workout with your kids they are not excuses', 'https://images.pexels.com/photos/3094230/pexels-photo-3094230.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', CURRENT_TIMESTAMP );
 
 CREATE TABLE comments(
   id SERIAL PRIMARY KEY,
